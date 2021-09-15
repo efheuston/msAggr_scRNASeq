589c22948c199f6a9782b4ca1eb68c59165b8f35
efheuston <efheuston@gmail.com>
2021-09-15 09:42:29 -0400

    add pseudobulk_automate.R

---
+++ pseudobulk_vs_bulk/pseudobulk_automate.R
@@ -0,0 +1,119 @@
+## Load libraries
+library(dplyr)
+library(Seurat)
+library(ggplot2)
+
+## Set global variables
+projectName <- "pseudobulk_allPops"
+cell.list <- c("LSKm2", "CMPm2", "CMPm", "MEPm", "GMPm")
+bulk.list <- c("LSK", "CMP", "CFUE", "CFUMK", "ERY", "GMP", "MK", "MEP")
+export.graphs <- TRUE
+all.comparisons <- TRUE
+
+## Store session info
+sessionInfo.filename <- paste0(projectName, "_sessionInfo.txt")
+sink(sessionInfo.filename)
+sessionInfo()
+sink()
+
+## Load local scripts
+source ("../RFunctions/ColorPalette.R")
+
+spearman.scriptseq <- data.frame(matrix(NA, nrow = length(bulk.list), ncol = length(cell.list)))
+rownames(spearman.scriptseq) <- bulk.list
+colnames(spearman.scriptseq) <- cell.list
+head(spearman.scriptseq)
+
+spearman.totalseq <- spearman.scriptseq
+
+
+for (cell.pop in cell.list){
+	# Load scRNASeq object
+	seurat.object <- readRDS(paste0("~/Desktop/10XGenomicsData/cellRanger/", cell.pop, "_raw.RDS"))
+	# Calculate cpm 
+	sc.mtx <- data.frame(rowSums(seurat.object@assays$RNA@counts))
+	sc.mtx <- sc.mtx/colSums(sc.mtx)*1000000
+	
+	if(all.comparisons == TRUE){
+		bulk.pops <- bulk.list
+	}else{
+		bulk.pops <- substr(cell.pop, 1, 3)
+	}
+	print(bulk.pops)	
+	for(comparison.pop in bulk.pops){
+		# Automate through bRNASeq data
+		bulk.folders <- c("TotalSeq", "ScriptSeq")
+		
+		for(sequencing.folder in bulk.folders){
+			bulk.subfolders <- c(paste0("~/Desktop/10XGenomicsData/bRNASeq/", sequencing.folder, "/"))
+			
+			for(bulk.path in bulk.subfolders){
+				bulk.path <- bulk.subfolders[1]
+				file_list <- list.files(path = bulk.path, pattern = comparison.pop)
+				bulk.pop <- read.table(file = paste0(bulk.path, file_list[1]), header = TRUE)[1]
+				
+				for(file in file_list){
+					bulk.pop <- merge(bulk.pop, read.table(file = paste0(bulk.path, file), header = TRUE, sep = "\t")[, c(1,5)], by = "gene_id")
+				}
+				
+				#calculate average tpm
+				bulk.pop[comparison.pop] <- rowMeans(x = bulk.pop[, c(2, 3)])
+				
+				# clean and check df
+				bulk.pop <- bulk.pop[, -c(2, 3)]
+				
+				
+				#calc log_rho's
+				bulk.rho <- log((bulk.pop[comparison.pop] + 1), 2) - log(colSums((bulk.pop[comparison.pop])+ 1), 2)
+				rownames(bulk.rho) <- bulk.pop$gene_id
+				colnames(bulk.rho) <- "bulk"
+				sc.rho <- log((sc.mtx + 1), 2) - log(colSums((sc.mtx+1)), 2)
+				colnames(sc.rho) <- "sc"
+				
+				# check all entries are numeric
+				if(is.numeric(bulk.rho[,1]) == FALSE){
+					print("WARNING: bulk.rho datafame contains non-numeric values")
+				}
+				if(is.numeric(sc.rho[,1]) == FALSE){
+					print("WARNING: sc.rho datafame contains non-numeric values")
+				}
+				
+				
+				# drop NAs
+				concordance.df <- merge(bulk.rho, sc.rho, by = "row.names", all = TRUE)
+				concordance.df <- na.omit(concordance.df)
+				rownames(concordance.df) <- concordance.df$Row.names
+				concordance.df <- concordance.df[, c(2, 3)]
+				
+				# spearman plot
+				spearman.coeff <- cor(concordance.df, method = "spearman")[1, 2]
+				if(sequencing.folder == "TotalSeq"){
+					spearman.totalseq[bulk.list, cell.list] <- spearman.coeff
+				}
+				if(sequencing.folder == "ScriptSeq"){
+					spearman.scriptseq[bulk.list, cell.list] <- spearman.coeff
+				}
+				
+				
+				
+				# cor.plot <- ggplot(data = concordance.df, aes(x = bulk, y = sc)) + 
+				# 	geom_point()  +
+				# 	ggtitle(paste0(cell.pop, "_vs_", comparison.pop, "-", sequencing.folder, ": spearman coeff = ", round(spearman.coeff, digits = 3)))
+				# plot(cor.plot)
+				# 
+				# 
+				# if(export.graphs == TRUE){
+				# 	png(filename = paste0(cell.pop, "_vs_", comparison.pop, "-", sequencing.folder,"-spearmanPlot.png"), height = 480, width = 800)
+				# 	plot(cor.plot)
+				# 	dev.off()
+				# } else{
+				# 	print("Export graphs set to FALSE")
+				# }
+			}
+			
+		}
+	}
+}
+
+
+
